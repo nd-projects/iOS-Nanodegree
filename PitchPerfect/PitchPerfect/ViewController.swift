6127511c8e7b81f2de82721e11d7df68d14239b9
Nicholas Dunning <Nicholas.Dunning@bmw.de>
2020-02-21 12:22:09 +0100

    Add UI icons, update buttons and toggle between states

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -11,18 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recodingLabel: UILabel!
+    @IBOutlet weak var startRecordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //
     }
 
     @IBAction func recordAudio(_ sender: Any) {
         recodingLabel.text = "Recording in Progress"
+        startRecordingButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         recodingLabel.text = "Tab to Record"
+        startRecordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
     
 }
