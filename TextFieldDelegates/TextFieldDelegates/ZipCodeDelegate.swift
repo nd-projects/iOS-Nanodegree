3fa4d85fd1fcece6deaa051980148266f6d46ecd
Nicholas Dunning <Nicholas.Dunning@bmw.de>
2020-02-26 15:47:32 +0100

    Create project for assigning custom delegates to text input fields

---
+++ TextFieldDelegates/TextFieldDelegates/ZipCodeDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  ZipCodeDelegate.swift
+//  TextFieldDelegates
+//
+//  Copyright © 2020 nd-projects. All rights reserved.
+//
+
+import UIKit
+
+class ZipCodeDelegate : NSObject, UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let textFieldText = textField.text,
+            let rangeOfTextToReplace = Range(range, in: textFieldText) else {
+                return false
+        }
+        let substringToReplace = textFieldText[rangeOfTextToReplace]
+        let count = textFieldText.count - substringToReplace.count + string.count
+        return count <= 5
+    }
+}
