7a8c91f3f269ffb168b402b0a85997fb132f92d2
Nicholas Dunning <Nicholas.Dunning@bmw.de>
2020-03-29 16:11:47 +0200

    Enable sharing functionality
    Also setup delegates for the 2 text fields

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -14,13 +14,17 @@ struct Meme {
     var memedImage: UIImage
 }
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
+                    UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var toolbar: UIToolbar!
+    var meme: Meme!
+    var defaultTextTop: String = "TOP"
+    var defaultTextBottom: String = "BOTTOM"
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -38,6 +42,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
                                                             target: self,
                                                             action: #selector(shareMeme))
+        navigationItem.rightBarButtonItem?.isEnabled = false
 
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
 
@@ -45,18 +50,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             NSAttributedString.Key.strokeColor: UIColor.black,
             NSAttributedString.Key.foregroundColor: UIColor.white,
             NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSAttributedString.Key.strokeWidth: 3.0
+            NSAttributedString.Key.strokeWidth: -3.0
         ]
 
         topTextField.defaultTextAttributes = memeTextAttributes
-        topTextField.text = "TOP TEXT"
+        topTextField.text = defaultTextTop
         topTextField.autocapitalizationType = .allCharacters
         topTextField.textAlignment = .center
+        topTextField.delegate = self
 
         bottomTextField.defaultTextAttributes = memeTextAttributes
-        bottomTextField.text = "BOTTOM TEXT"
+        bottomTextField.text = defaultTextBottom
         bottomTextField.autocapitalizationType = .allCharacters
         bottomTextField.textAlignment = .center
+        bottomTextField.delegate = self
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case topTextField:
+            if textField.text == defaultTextTop {
+                textField.text = ""
+            }
+        case bottomTextField:
+            if textField.text == defaultTextBottom {
+                textField.text = ""
+            }
+        default:
+            return
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+
+        return true
     }
 
     @IBAction func pickAnImage(_ sender: Any) {
@@ -78,7 +106,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
         if let pickedImage = info[.originalImage] as? UIImage {
             imagePickerView.image = pickedImage
-            // @todo: enable share button once an image is selected
+            navigationItem.rightBarButtonItem?.isEnabled = true
         }
     }
 
@@ -136,15 +164,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     @objc func shareMeme() {
-        // @todo: change the shared image to the completed meme
-        let activityViewController = UIActivityViewController(activityItems: [imagePickerView.image!],
+        updateMeme()
+        let activityViewController = UIActivityViewController(activityItems: [self.meme.memedImage],
                                                               applicationActivities: [])
         present(activityViewController, animated: true)
     }
 
-    func save() {
+    func updateMeme() {
         let memedImage = generateMemedImage()
-        let meme = Meme(topText: topTextField.text!,
+        self.meme = Meme(topText: topTextField.text!,
                         bottomText: bottomTextField.text!,
                         originalImage: imagePickerView.image!,
                         memedImage: memedImage)
