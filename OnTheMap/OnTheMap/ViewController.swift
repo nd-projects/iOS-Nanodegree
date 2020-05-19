ac1db696a479b115eb42ab351d893b84333e6b23
Nicholas Dunning <Nicholas.Dunning@bmw.de>
2020-05-19 10:48:41 +0200

    Create On The Map project

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Dunning Nicholas, EV-44 on 19.05.20.
+//  Copyright © 2020 nd-projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
